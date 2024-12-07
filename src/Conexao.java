c9234468ef71063ceeb910974eb07f9d05661a38
Lucas Barros <lbs12@aluno.ifal.edu.br>
2024-12-07 18:22:43 -0300

    add: crud usuário e instrutor e interface swing

--- src/Conexao.java
+++ src/Conexao.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 
 public class Conexao {
     private static final String URL = "jdbc:mysql://localhost:3306/{seu_banco}";
-    private static final String USUARIO = "{seu_usuario}";
+    private static final String USUARIO = "{seu_usuário}";
     private static final String SENHA = "{sua_senha}";
 
     public static Connection getConnection() {
